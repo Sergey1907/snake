d56625ee3467add9ab9485feb2ec985fd42f9715
Sergey1907 <serg_vlad@e1.ru>
2017-04-04 00:22:30 +0500

    snake horizontal/vertical lines

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,21 +16,11 @@ namespace snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            foreach (int i in numList) 
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
+            VerticalLine line1 = new VerticalLine(11, 20, 25, '#');
+            line1.Drow();
 
             Console.ReadLine();
         }
