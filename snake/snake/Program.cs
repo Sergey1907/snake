2d4d9fe922f882fb24b9ba0d2e4db6662f4d0bfb
Sergey1907 <serg_vlad@e1.ru>
2017-04-10 00:37:41 +0500

    snake with food

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -28,14 +28,30 @@ namespace snake
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
             while (true) 
             {
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else 
+                {
+                    snake.Move();
+                }
+
                 if (Console.KeyAvailable) 
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(300);
+                Thread.Sleep(900);
+                snake.Move();
+                Thread.Sleep(900);
                 snake.Move();
             }
         }
